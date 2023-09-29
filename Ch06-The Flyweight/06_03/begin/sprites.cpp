df6b2d19861375c12440b72a74a50db5897e1a09
Ashutosh Sameer <sameerashutosh0201@gmail.com>
2023-09-28 21:52:08 -0400

    Flyweight

--- Ch06-The Flyweight/06_03/begin/sprites.cpp
+++ Ch06-The Flyweight/06_03/begin/sprites.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -25,7 +26,18 @@ private:
 class Sprite
 {
 public:
-    Sprite(int width, int height, int x, int y, const string &textureFileName) : m_Width(width), m_Height(height), m_X(x), m_Y(y), m_Texture(new Texture(textureFileName)) {}
+    Sprite(const Texture *texture) : m_Texture(texture)
+    {
+        cout << "Creating sprite with texture: " << m_Texture->description() << endl;
+    }
+
+    void setPositionSize(int x, int y, int width, int height)
+    {
+        m_X = x;
+        m_Y = y;
+        m_Height = height;
+        m_Width = width;
+    }
 
     void render()
     {
@@ -33,19 +45,64 @@ public:
         cout << "Rendering sprite with texture: " << m_Texture->description() << endl;
     }
 
-    ~Sprite()
-    {
-        cout << "Destructing sprite with texture " << m_Texture->description() << endl;
-        delete m_Texture;
-    }
+    // ~Sprite()
+    // {
+    //     cout << "Destructing sprite with texture " << m_Texture->description() << endl;
+    //     delete m_Texture;
+    // }
 
 private:
-    const int m_Width;
-    const int m_Height;
-    const int m_X;
-    const int m_Y;
+    int m_Width;
+    int m_Height;
+    int m_X;
+    int m_Y;
 
     const Texture *m_Texture;
+};
+
+class SpriteFactory
+{
+public:
+    Sprite* makeSprite(const string& filename)
+    {
+        auto it = d_spritePool.find(filename);
+        if (it != d_spritePool.end())
+        {
+            return it->second;
+        }
+
+        const auto texture = getTexture(filename);
+        auto [newIt, _] = d_spritePool.emplace(filename, new Sprite(texture));
+        return newIt->second;
+    }
+
+    ~SpriteFactory()
+    {
+        // cout << "Destructing sprite with texture " << m_Texture->description() << endl;
+        // delete m_Texture;
+        for (auto &[filename, sprite] : d_spritePool)
+        {
+            delete sprite;
+        }
+        for (auto &[filename, texture] : d_texturePool)
+        {
+            delete texture;
+        }
+    }
+private:
+    unordered_map<string, Sprite*> d_spritePool;
+    unordered_map<string, Texture*> d_texturePool;
+
+    const Texture* getTexture(const string& filename)
+    {
+        auto it = d_texturePool.find(filename);
+        if (it != d_texturePool.end())
+        {
+            return it->second;
+        }
+        auto [newIt, _] = d_texturePool.emplace(filename, new Texture(filename));
+        return newIt->second;
+    }
 };
 
 int main()
@@ -55,9 +112,13 @@ int main()
     const int numSprites = 10;
     const string textureFile = "spaceship.png";
 
+    SpriteFactory sf;
+
     for (int i = 0; i < numSprites; ++i)
     {
-        auto sprite = new Sprite(10, 10, i * 10, i * 10, textureFile);
+        auto sprite = sf.makeSprite(textureFile);
+        sprite->setPositionSize(10, 10, i * 10, i * 10);
+        // new Sprite(10, 10, i * 10, i * 10, textureFile);
         sprites.push_back(sprite);
     }
 
@@ -67,11 +128,11 @@ int main()
         sprite->render();
     }
 
-    // cleanup
-    for (auto &sprite : sprites)
-    {
-        delete(sprite);
-    }
+    // // cleanup
+    // for (auto &sprite : sprites)
+    // {
+    //     delete(sprite);
+    // }
 
     return 0;
 }
